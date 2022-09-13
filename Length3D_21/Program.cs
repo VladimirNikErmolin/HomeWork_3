d6b86b1939c346bb188f10973dcecf8f0856fe63
Vladimir Ermolin <w.ermolin@yahoo.com>
2022-09-13 19:40:58 +0300

    length 3d

---
+++ Length3D_21/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите через пробел  координаты X Y Z точки А:");
+string xAyAzA = Console.ReadLine ()!; 
+string [] partsA = xAyAzA.Split(' ');  
+int xA = int.Parse (partsA[0]); 
+int yA = int.Parse (partsA[1]);
+int zA = int.Parse (partsA[2]);
+
+Console.WriteLine("Введите через пробел  координаты X Y Z точки B:");
+string xByBzB = Console.ReadLine ()!; 
+string [] partsB = xByBzB.Split(' ');  
+int xB = int.Parse (partsB[0]); 
+int yB = int.Parse (partsB[1]);
+int zB = int.Parse (partsB[2]);
+
+double distance = Math.Sqrt(Math.Pow(xA - xB, 2) + Math.Pow(yA - yB, 2) + Math.Pow(zA - zB, 2));
+Console.WriteLine($"Расстояние между двумя точками равно {distance:F2} единиц.");
